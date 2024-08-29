33ad53da8e634bed906d9b45f17efd53d7eb87e5
DarynaLichman <daryna.lichman@tech-stack.io>
2024-08-29 16:24:42 +0300

    Change workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   globalTimeout: 60000,
   testDir: "./tests",
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: "html",
   use: {
     baseURL: "https://traineeautomation.azurewebsites.net/",
